db26e2350128fe4f9ca98ff1ead1a0ce839d61f3
Marvin <marvinnrn@gmail.com>
2022-05-19 05:24:02 +0200

    exemple final attendu pour la page d'accueil

--- groupomania-project/src/components/PostMsg.vue
+++ groupomania-project/src/components/PostMsg.vue
@@ -5,7 +5,9 @@
                 <img class="profile__photo" src="../assets/Groupomania_Logos+(3)/icon.png" alt="photo de profile" />
                 <h2 class="profile__name">Nom d'utilisateur</h2>
             </div>
-            <div class="textarea" contenteditable="true" placeholder="Quoi de neuf ?" />
+            <div class="post">
+                <span class="post__content" contenteditable="true" placeholder="Quoi de neuf ?" />
+            </div>
             <div class="post__btns">
                 <button class="addImg__btn">
                     <i class="fa-solid fa-images"></i> image
--- groupomania-project/src/components/ShowAllPost.vue
+++ groupomania-project/src/components/ShowAllPost.vue
@@ -14,6 +14,24 @@
                 rerum! Magni, libero. Eius, vero quis illum ea voluptatum vitae, dolor esse facilis maiores fugit eum
                 excepturi exercitationem quaerat asperiores, ipsa reiciendis! Deleniti, incidunt ratione officiis quod
                 accusamus aliquid dignissimos tenetur provident.
+                <div class="like__section">
+                    <p><i class="fa-solid fa-heart like__btn"></i>1K</p>
+                    <p class="post__date">Il y a 8 jours</p>
+                </div>
+            </div>
+            <div class="comments ">
+                <img class="profile__photo" src="../assets/Groupomania_Logos+(3)/icon.png" alt="photo de profile" />
+                <p class="comments__comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti optio
+                    quae mollitia praesentium
+                    maiores pariatur fuga. Unde enim nobis facere ipsa natus explicabo repudiandae, modi expedita, eius
+                    nostrum, officiis praesentium!
+                </p>
+            </div>
+            <p class="comments__date">Il y a 5 jours</p>
+            <div class="reply ">
+                <img class="profile__photo" src="../assets/Groupomania_Logos+(3)/icon.png" alt="photo de profile" />
+                <span class="reply__field" contenteditable="true" placeholder="Écrivez votre réponse..." />
+                <button class="reply__btn"><i class="fa-solid fa-reply"></i></button>
             </div>
         </div>
     </div>
